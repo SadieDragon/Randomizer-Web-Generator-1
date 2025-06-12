10c8198598b84cda4cf96b82d43e26ecfc90dc81
SadieDragon <53710912+SadieDragon@users.noreply.github.com>
2025-06-12 12:06:48 -0500

    remove a random `using` that popped up

--- Generator/Logic/LogicFunctions/HelperFunctions.cs
+++ Generator/Logic/LogicFunctions/HelperFunctions.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.Metadata;
 using TPRandomizer;
 using TPRandomizer.SSettings.Enums;
 using ERLF = LogicFunctionsNS.ERLogicFunctions;
