04594c3a416ac9b6d7f075e932d1fc081ea009ef
SadieDragon <53710912+SadieDragon@users.noreply.github.com>
2025-06-16 22:54:05 -0500

    remove unused imports I forgot about
    Sorry

--- Generator/Logic/LogicFunctions/HelperFunctions.cs
+++ Generator/Logic/LogicFunctions/HelperFunctions.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using TPRandomizer;
 using TPRandomizer.SSettings.Enums;
-using CUU = LogicFunctionsNS.CanUseUtilities;
 using HHSL = LogicFunctionsNS.HasHiddenSkillLevel;
 using LF = TPRandomizer.LogicFunctionsUpdatedRefactored;
 
