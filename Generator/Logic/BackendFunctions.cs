c3afb226b3ca1a921aa54bfba8b152d06d01134d
lunarsoap5 <justindakotacarter@gmail.com>
2025-07-14 08:35:01 -0500

    Fix spoiler/sphere issue and added rando files for ct spawn

--- Generator/Logic/BackendFunctions.cs
+++ Generator/Logic/BackendFunctions.cs
@@ -456,9 +456,10 @@ namespace TPRandomizer
                 sphereCount++;
                 if ((hasCompletedSphere == false) && !hasConcludedPlaythrough)
                 {
-                    throw new Exception(
+                    Console.WriteLine(
                         "Could not validate playthrough. There possibly is an error in logic or the specific playthrough has failed."
                     );
+                    break;
                 }
             }
 
