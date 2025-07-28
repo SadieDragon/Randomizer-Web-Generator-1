5cd8874dbc1eab493474a5c8f3c65279667704d3
SadieDragon <53710912+SadieDragon@users.noreply.github.com>
2025-07-28 00:02:45 -0500

    pull replenish utils out into a separate file
    Mostly for the file length, but also just for clarity- they aren't "Can I use this item?" but rather "Can I refill this item?"

---
+++ Generator/Logic/LogicFunctions/Utils/HasItemUtils/CanReplenishUtils.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using TPRandomizer;
+
+namespace LogicFunctionsNS
+{
+    public static class CanReplenishUtils
+    {
+        public static bool CanReplenishItem(Item item)
+        {
+            Dictionary<Item, bool> itemRefills = new()
+            {
+                { Item.Lantern, CanRefillOil() },
+                { Item.Progressive_Bow, CanGetArrows() },
+            };
+
+            if (itemRefills.TryGetValue(item, out var check))
+            {
+                return check;
+            }
+            return true;
+        }
+
+        public static bool CanGetArrows()
+        {
+            return ERLogicFunctions.HasReachedRoom("Lost Woods")
+                || (
+                    CanCompleteDungeon.CanCompleteGoronMines()
+                    && ERLogicFunctions.HasReachedRoom("Kakariko Malo Mart")
+                )
+                || ERLogicFunctions.CanShopFromRoom("Castle Town Goron House Balcony");
+        }
+
+        public static bool CanRefillOil()
+        {
+            return ERLogicFunctions.HasReachedRoom("North Faron Woods")
+                || ERLogicFunctions.HasReachedRoom("South Faron Woods")
+                || ERLogicFunctions.HasReachedRoom("Arbiters Grounds Entrance")
+                || (ERLogicFunctions.HasReachedRoom("Lake Hylia Long Cave") && BombUtils.CanSmash())
+                || ERLogicFunctions.HasReachedRoom("Ordon Seras Shop")
+                || (
+                    CanCompleteDungeon.CanCompleteGoronMines()
+                    && ERLogicFunctions.HasReachedRoom("Lower Kakariko Village")
+                    && CanDoStuff.CanChangeTime()
+                )
+                || ERLogicFunctions.CanShopFromRoom("Castle Town Goron House")
+                || ERLogicFunctions.HasReachedRoom("Death Mountain Hot Spring")
+                || ERLogicFunctions.HasReachedRoom("City in The Sky Entrance")
+                // This is for lantern staircase room
+                || (
+                    ERLogicFunctions.HasReachedRoom("Hyrule Castle Main Hall")
+                    && CanDefeatCommonEnemy.CanDefeatBokoblin()
+                    && CanDefeatCommonEnemy.CanDefeatLizalfos()
+                    && HasClawshotCount.HasDoubleClawshot()
+                    && CanDefeatCommonEnemy.CanDefeatDarknut()
+                )
+                || (
+                    ERLogicFunctions.HasReachedRoom("Eldin Lantern Cave")
+                    && MiscItemUtils.CanBurnWebs()
+                    && CanDefeatCommonEnemy.CanDefeatChu()
+                );
+        }
+    }
+}
--- Generator/Logic/LogicFunctions/Utils/HasItemUtils/CanUseUtils.cs
+++ Generator/Logic/LogicFunctions/Utils/HasItemUtils/CanUseUtils.cs
@@ -11,26 +11,11 @@ namespace LogicFunctionsNS
 
         public static bool CanUse(Item item)
         {
-            return ItemList.Contains(item) && CanReplenishItem(item);
+            return ItemList.Contains(item) && CanReplenishUtils.CanReplenishItem(item);
         }
 
         // unused override for passing in a str.
         public static bool CanUse(string item) => CanUse(HelperFunctions.ConvertStrToItem(item));
-
-        public static bool CanReplenishItem(Item item)
-        {
-            Dictionary<Item, bool> itemRefills = new()
-            {
-                { Item.Lantern, CanRefillOil() },
-                { Item.Progressive_Bow, CanGetArrows() },
-            };
-
-            if (itemRefills.TryGetValue(item, out var check))
-            {
-                return check;
-            }
-            return true;
-        }
 
         private static int CountItem(Item itemToBeCounted)
         {
@@ -46,7 +31,7 @@ namespace LogicFunctionsNS
         public static int GetItemCount(Item itemToBeCounted)
         {
             // Only count how many of the item there are if it can be replenished
-            if (CanReplenishItem(itemToBeCounted))
+            if (CanReplenishUtils.CanReplenishItem(itemToBeCounted))
             {
                 return CountItem(itemToBeCounted);
             }
@@ -63,47 +48,6 @@ namespace LogicFunctionsNS
         public static bool VerifyItemQuantity(string itemToBeCounted, int quantity)
         {
             return VerifyItemQuantity(HelperFunctions.ConvertStrToItem(itemToBeCounted), quantity);
-        }
-
-        // I put these here because they go with CanReplenish.
-        public static bool CanGetArrows()
-        {
-            return ERLogicFunctions.HasReachedRoom("Lost Woods")
-                || (
-                    CanCompleteDungeon.CanCompleteGoronMines()
-                    && ERLogicFunctions.HasReachedRoom("Kakariko Malo Mart")
-                )
-                || ERLogicFunctions.CanShopFromRoom("Castle Town Goron House Balcony");
-        }
-
-        public static bool CanRefillOil()
-        {
-            return ERLogicFunctions.HasReachedRoom("North Faron Woods")
-                || ERLogicFunctions.HasReachedRoom("South Faron Woods")
-                || ERLogicFunctions.HasReachedRoom("Arbiters Grounds Entrance")
-                || (ERLogicFunctions.HasReachedRoom("Lake Hylia Long Cave") && BombUtils.CanSmash())
-                || ERLogicFunctions.HasReachedRoom("Ordon Seras Shop")
-                || (
-                    CanCompleteDungeon.CanCompleteGoronMines()
-                    && ERLogicFunctions.HasReachedRoom("Lower Kakariko Village")
-                    && CanDoStuff.CanChangeTime()
-                )
-                || ERLogicFunctions.CanShopFromRoom("Castle Town Goron House")
-                || ERLogicFunctions.HasReachedRoom("Death Mountain Hot Spring")
-                || ERLogicFunctions.HasReachedRoom("City in The Sky Entrance")
-                // This is for lantern staircase room
-                || (
-                    ERLogicFunctions.HasReachedRoom("Hyrule Castle Main Hall")
-                    && CanDefeatCommonEnemy.CanDefeatBokoblin()
-                    && CanDefeatCommonEnemy.CanDefeatLizalfos()
-                    && HasClawshotCount.HasDoubleClawshot()
-                    && CanDefeatCommonEnemy.CanDefeatDarknut()
-                )
-                || (
-                    ERLogicFunctions.HasReachedRoom("Eldin Lantern Cave")
-                    && MiscItemUtils.CanBurnWebs()
-                    && CanDefeatCommonEnemy.CanDefeatChu()
-                );
         }
     }
 }
